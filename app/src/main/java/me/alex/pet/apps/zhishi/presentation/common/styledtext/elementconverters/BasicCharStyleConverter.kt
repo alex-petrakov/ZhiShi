4eef64f933a3c637fa5cddcdf19f3f6d9ceb3708
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-02-25 22:59:15 +0300

    Add BasicCharStyleConverter

---
+++ app/src/main/java/me/alex/pet/apps/zhishi/presentation/common/styledtext/elementconverters/BasicCharStyleConverter.kt
@@ -0,0 +1,19 @@
+package me.alex.pet.apps.zhishi.presentation.common.styledtext.elementconverters
+
+import android.graphics.Typeface
+import android.text.style.StyleSpan
+import me.alex.pet.apps.zhishi.domain.CharacterStyle
+import me.alex.pet.apps.zhishi.domain.CharacterStyleType
+import me.alex.pet.apps.zhishi.presentation.common.styledtext.PositionAwareSpan
+
+class BasicCharStyleConverter : ElementConverter<CharacterStyle> {
+
+    override fun convertToSpan(element: CharacterStyle): PositionAwareSpan? {
+        val span = when (element.type) {
+            CharacterStyleType.EMPHASIS -> StyleSpan(Typeface.ITALIC)
+            CharacterStyleType.STRONG_EMPHASIS -> StyleSpan(Typeface.BOLD)
+            CharacterStyleType.MISSPELL -> null
+        }
+        return span?.let { PositionAwareSpan(span, element.start, element.end) }
+    }
+}
