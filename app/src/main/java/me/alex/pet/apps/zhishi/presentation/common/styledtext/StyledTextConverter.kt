501f248b32a768fc39e452d4b217f198ae69e6bb
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-02-26 00:44:07 +0300

    Fix rendering of indented quote paragraphs

--- app/src/main/java/me/alex/pet/apps/zhishi/presentation/common/styledtext/StyledTextConverter.kt
+++ app/src/main/java/me/alex/pet/apps/zhishi/presentation/common/styledtext/StyledTextConverter.kt
@@ -7,18 +7,20 @@ import me.alex.pet.apps.zhishi.domain.*
 import me.alex.pet.apps.zhishi.presentation.common.styledtext.elementconverters.ElementConverter
 
 class StyledTextConverter(
-        private val characterStyleConverter: ElementConverter<CharacterStyle>? = null,
-        private val linkConverter: ElementConverter<Link>? = null,
         private val paragraphStyleConverter: ElementConverter<ParagraphStyle>? = null,
-        private val indentConverter: ElementConverter<Indent>? = null
+        private val indentConverter: ElementConverter<Indent>? = null,
+        private val characterStyleConverter: ElementConverter<CharacterStyle>? = null,
+        private val linkConverter: ElementConverter<Link>? = null
 ) {
     fun convertToSpanned(styledText: StyledText): Spanned {
         val spans = mutableListOf<PositionAwareSpan>()
         styledText.run {
             characterStyles.mapNotNullTo(spans) { characterStyleConverter?.convertToSpan(it) }
             links.mapNotNullTo(spans) { linkConverter?.convertToSpan(it) }
-            paragraphStyles.mapNotNullTo(spans) { paragraphStyleConverter?.convertToSpan(it) }
+            // Indents must be rendered before paragraph spans in order for QuotationSpans to be
+            // applied correctly
             indents.mapNotNullTo(spans) { indentConverter?.convertToSpan(it) }
+            paragraphStyles.mapNotNullTo(spans) { paragraphStyleConverter?.convertToSpan(it) }
         }
 
         return SpannableString(styledText.string).apply {
