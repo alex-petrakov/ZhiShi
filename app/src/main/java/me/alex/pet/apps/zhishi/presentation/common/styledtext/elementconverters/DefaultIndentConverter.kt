af2b9b6723f6ae476c3fa0e281a7f24b81387375
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-02-25 23:56:17 +0300

    Change IndentSpan size measuring method
    Update IndentSpan so that it determines indent size based on text paint properties.

--- app/src/main/java/me/alex/pet/apps/zhishi/presentation/common/styledtext/elementconverters/DefaultIndentConverter.kt
+++ app/src/main/java/me/alex/pet/apps/zhishi/presentation/common/styledtext/elementconverters/DefaultIndentConverter.kt
@@ -1,31 +1,55 @@
 package me.alex.pet.apps.zhishi.presentation.common.styledtext.elementconverters
 
-import android.content.res.Resources
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.text.Layout
 import android.text.style.LeadingMarginSpan
 import androidx.annotation.Px
 import me.alex.pet.apps.zhishi.domain.Indent
 import me.alex.pet.apps.zhishi.presentation.common.styledtext.PositionAwareSpan
 import kotlin.math.roundToInt
 
-class DefaultIndentConverter(resources: Resources) : ElementConverter<Indent> {
-
-    @Px
-    private val indentStep: Int = 16.dp(resources)
+class DefaultIndentConverter : ElementConverter<Indent> {
 
     override fun convertToSpan(element: Indent): PositionAwareSpan? {
-        return PositionAwareSpan(IndentSpan(element.level, indentStep), element.start, element.end)
+        return PositionAwareSpan(IndentSpan(element.level), element.start, element.end)
     }
 }
 
-private fun Int.dp(resources: Resources): Int {
-    return (this * resources.displayMetrics.density).roundToInt()
-}
+data class IndentSpan(private val level: Int) : LeadingMarginSpan {
 
-class IndentSpan(level: Int, @Px step: Int) : LeadingMarginSpan.Standard(step * level) {
+    @Px
+    private var step: Int = 0
+
     init {
-        require(level in minLevel..maxLevel) { "Level must be in [$minLevel, $maxLevel], but it was $level" }
+        require(level in minLevel..maxLevel) {
+            "Level must be in [$minLevel, $maxLevel], but it was $level"
+        }
+    }
+
+    override fun drawLeadingMargin(
+            c: Canvas,
+            p: Paint,
+            x: Int,
+            dir: Int,
+            top: Int,
+            baseline: Int,
+            bottom: Int,
+            text: CharSequence,
+            start: Int,
+            end: Int,
+            first: Boolean,
+            layout: Layout
+    ) {
+        step = p.measureText(indentStepLengthSample).roundToInt()
+    }
+
+    override fun getLeadingMargin(first: Boolean): Int {
+        return step * level
     }
 }
+
+private const val indentStepLengthSample = "   "
 
 private const val minLevel = 1
 private const val maxLevel = 5
