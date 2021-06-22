b5ffb438576786eb90e27c79d6a9437874a6e010
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-06-22 18:29:29 +0300

    Implement equals, hashCode and toString for ParagraphSpan

--- app/src/main/java/me/alex/pet/apps/zhishi/domain/common/ParagraphSpan.kt
+++ app/src/main/java/me/alex/pet/apps/zhishi/domain/common/ParagraphSpan.kt
@@ -11,9 +11,67 @@ sealed class ParagraphSpan(val start: Int, val end: Int) {
         init {
             require(level >= 0)
         }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Indent) return false
+            if (!super.equals(other)) return false
+
+            if (level != other.level) return false
+            if (hangingText != other.hangingText) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = super.hashCode()
+            result = 31 * result + level
+            result = 31 * result + hangingText.hashCode()
+            return result
+        }
+
+        override fun toString(): String {
+            return "Indent(start=$start, end=$end, level=$level, hangingText='$hangingText')"
+        }
     }
 
-    class Style(start: Int, end: Int, val appearance: ParagraphAppearance) : ParagraphSpan(start, end)
+    class Style(start: Int, end: Int, val appearance: ParagraphAppearance) : ParagraphSpan(start, end) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Style) return false
+            if (!super.equals(other)) return false
+
+            if (appearance != other.appearance) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = super.hashCode()
+            result = 31 * result + appearance.hashCode()
+            return result
+        }
+
+        override fun toString(): String {
+            return "Style(start=$start, end=$end, appearance=$appearance)"
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ParagraphSpan) return false
+
+        if (start != other.start) return false
+        if (end != other.end) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = start
+        result = 31 * result + end
+        return result
+    }
 }
 
 enum class ParagraphAppearance {
