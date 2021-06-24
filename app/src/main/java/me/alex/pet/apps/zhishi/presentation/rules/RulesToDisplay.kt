833128162f638e0de103611027099ab25ef12bf2
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-06-24 18:26:49 +0300

    Use kotlin-parcelize to generate Parcelable implementations

--- app/src/main/java/me/alex/pet/apps/zhishi/presentation/rules/RulesToDisplay.kt
+++ app/src/main/java/me/alex/pet/apps/zhishi/presentation/rules/RulesToDisplay.kt
@@ -1,8 +1,9 @@
 package me.alex.pet.apps.zhishi.presentation.rules
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class RulesToDisplay(val ids: List<Long>, val selectionIndex: Int) : Parcelable {
 
     init {
@@ -11,38 +12,4 @@ data class RulesToDisplay(val ids: List<Long>, val selectionIndex: Int) : Parcel
     }
 
     constructor(ruleId: Long) : this(listOf(ruleId), 0)
-
-    private constructor(parcel: Parcel) : this(
-            parcel.readListOfLong(),
-            parcel.readInt()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeListOfLong(ids)
-        dest.writeInt(selectionIndex)
-    }
-
-    companion object CREATOR : Parcelable.Creator<RulesToDisplay> {
-        override fun createFromParcel(parcel: Parcel): RulesToDisplay {
-            return RulesToDisplay(parcel)
-        }
-
-        override fun newArray(size: Int): Array<RulesToDisplay?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
-
-private fun Parcel.readListOfLong(): List<Long> {
-    val size = readInt()
-    val arr = LongArray(size)
-    readLongArray(arr)
-    return arr.toList()
-}
-
-private fun Parcel.writeListOfLong(list: List<Long>) {
-    writeInt(list.size)
-    writeLongArray(list.toLongArray())
 }
