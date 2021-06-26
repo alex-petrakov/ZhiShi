fafd953f80d9f12ed5bd1740fc801a21299478f1
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-06-26 18:26:50 +0300

    Add material transitions

--- app/src/main/java/me/alex/pet/apps/zhishi/presentation/HostActivity.kt
+++ app/src/main/java/me/alex/pet/apps/zhishi/presentation/HostActivity.kt
@@ -2,16 +2,36 @@ package me.alex.pet.apps.zhishi.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.github.terrakok.cicerone.NavigatorHolder
 import com.github.terrakok.cicerone.Replace
 import com.github.terrakok.cicerone.androidx.AppNavigator
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import com.google.android.material.transition.MaterialSharedAxis
 import me.alex.pet.apps.zhishi.R
 import me.alex.pet.apps.zhishi.databinding.ActivityHostBinding
 import org.koin.android.ext.android.inject
 
 class HostActivity : AppCompatActivity() {
 
-    private val navigator = AppNavigator(this, R.id.fragment_container)
+    private val navigator = object : AppNavigator(this, R.id.fragment_container) {
+        override fun setupFragmentTransaction(
+                screen: FragmentScreen,
+                fragmentTransaction: FragmentTransaction,
+                currentFragment: Fragment?,
+                nextFragment: Fragment
+        ) {
+            currentFragment?.apply {
+                exitTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+                reenterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
+            }
+            nextFragment.apply {
+                enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true)
+                returnTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false)
+            }
+        }
+    }
 
     private val navigatorHolder by inject<NavigatorHolder>()
 
