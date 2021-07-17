0b8559d62e32cd155c7fba734d095de2beeb64e4
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-07-17 17:04:16 +0300

    Fix the way hanging footnote text is measured

--- app/src/main/java/me/alex/pet/apps/zhishi/presentation/common/styledtext/spanrenderers/ParagraphSpanRenderer.kt
+++ app/src/main/java/me/alex/pet/apps/zhishi/presentation/common/styledtext/spanrenderers/ParagraphSpanRenderer.kt
@@ -23,9 +23,14 @@ class ParagraphSpanRenderer(
 
     private val quoteStripeWidth = 2.dp(theme.resources)
 
-    private val quoteBorderGapWidth = textPaint.measureText(quoteGapSample).roundToInt()
+    private val quoteGapWidth = textPaint.measureText(quoteGapSample).roundToInt()
 
     private val footnoteTextSize = 15.sp(theme.resources)
+
+    private val footnoteTextPaint = TextPaint().apply {
+        set(textPaint)
+        textSize = footnoteTextSize.toFloat()
+    }
 
     override fun convertToAndroidSpans(spans: List<ParagraphSpan>): List<PositionAwareSpan> {
         return spans.flatMap { convertToAndroidSpans(it) }
@@ -46,21 +51,52 @@ class ParagraphSpanRenderer(
             span.hasInnerIndent -> ""
             else -> span.indent.hangingText
         }
-        return newIndent(span.start, span.end, span.indent.outer, hangingText)
+        return newIndent(
+                span.start,
+                span.end,
+                span.indent.outer,
+                hangingText,
+                getTextPaintFor(span)
+        )
     }
 
     private fun convertToInnerIndentSpan(span: ParagraphSpan): PositionAwareSpan? {
         if (!span.hasInnerIndent) {
             return null
         }
-        return newIndent(span.start, span.end, span.indent.inner, span.indent.hangingText)
+        return newIndent(
+                span.start,
+                span.end,
+                span.indent.inner,
+                span.indent.hangingText,
+                getTextPaintFor(span)
+        )
     }
+
+    private fun getTextPaintFor(span: ParagraphSpan): TextPaint {
+        return when {
+            span.isFootnote -> footnoteTextPaint
+            else -> textPaint
+        }
+    }
+
+    private val ParagraphSpan.isFootnote: Boolean
+        get() {
+            return this.appearance == ParagraphAppearance.FOOTNOTE ||
+                    this.appearance == ParagraphAppearance.FOOTNOTE_QUOTE
+        }
 
     private val ParagraphSpan.hasInnerIndent get() = indent.inner > 0
 
     private val ParagraphSpan.hasOuterIndent get() = indent.outer > 0
 
-    private fun newIndent(start: Int, end: Int, level: Int, hangingText: String): PositionAwareSpan {
+    private fun newIndent(
+            start: Int,
+            end: Int,
+            level: Int,
+            hangingText: String,
+            textPaint: TextPaint
+    ): PositionAwareSpan {
         require(level > 0)
         val restLinesIndent = indentStepWidth * level
         val hangingTextWidth = textPaint.measureText(hangingText)
@@ -84,7 +120,7 @@ class ParagraphSpanRenderer(
     }
 
     private fun newQuote(start: Int, end: Int): PositionAwareSpan {
-        val quotationSpan = QuotationSpan(stripeColor, quoteStripeWidth, quoteBorderGapWidth)
+        val quotationSpan = QuotationSpan(stripeColor, quoteStripeWidth, quoteGapWidth)
         return PositionAwareSpan(quotationSpan, start, end)
     }
 
