de6874fae8f0cfa91d8cad51976b1a80e0cb185f
Alex Petrakov <alex.petrakov.dev@gmail.com>
2021-09-03 17:02:35 +0300

    Use onViewCreated instead of onActivityCreated in LicensesFragment

--- app/src/main/java/me/alex/pet/apps/zhishi/presentation/licenses/LicensesFragment.kt
+++ app/src/main/java/me/alex/pet/apps/zhishi/presentation/licenses/LicensesFragment.kt
@@ -17,13 +17,17 @@ class LicensesFragment : Fragment() {
 
     private val router by inject<Router>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentLicensesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         prepareView()
     }
 
